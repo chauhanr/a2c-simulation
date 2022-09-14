e2e4dc23cf64f3ff48feba464d8a44cad16bf5ab
developer1 <>
2022-09-14 09:14:11 +0000

    {/src/main/java/com/myspace/a2c_simulation/Sample.java}

--- src/main/java/com/myspace/a2c_simulation/Sample.java
+++ src/main/java/com/myspace/a2c_simulation/Sample.java
@@ -8,8 +8,11 @@ public class Sample implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@org.kie.api.definition.type.Label(value = "name")
+	@org.kie.api.definition.type.Label("name")
 	private java.lang.String name;
+
+	@org.kie.api.definition.type.Label(value = "age")
+	private java.lang.Integer age;
 
 	public Sample() {
 	}
@@ -22,8 +25,17 @@ public class Sample implements java.io.Serializable {
 		this.name = name;
 	}
 
-	public Sample(java.lang.String name) {
+	public java.lang.Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(java.lang.Integer age) {
+		this.age = age;
+	}
+
+	public Sample(java.lang.String name, java.lang.Integer age) {
 		this.name = name;
+		this.age = age;
 	}
 
 }
